b0ef2e0272ed3ab70c19d6f81e4a41ac1cb355ca
Elfego Juarez <elfegoadair@gmail.com>
2022-06-29 18:16:44 -0500

    Primer Commit

---
+++ SopaL.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void cargar (char palabras[10][50])
+{
+	int f;
+	for(f=0; f<10;f++){
+		printf("Ingrese la palabra: ");
+		gets(palabras[f]);
+	}
+}
+
+void imprimir(char palabras[10][50]){
+	int f;
+	printf("Las palabras ingresadas son:\n");
+	
+	for(f=0; f<10;f++){
+		printf("%s\n", palabras[f]);
+		}
+	
+	
+}
+
+void consulta(char palabras[10][50]){
+	int f;
+	char pal[50];
+	int existe=0;
+	printf("Ingrese la palabra a buscar: ");
+	gets(pal);
+	for(f=0;f<10;f++){
+		if(strcmp(pal,palabras[f])==0){
+			existe=1;
+		}
+	}
+	
+	if(existe==1){
+		printf("La palabra esta en la matriz");
+	} else{
+		printf("La palabra no esta en la matriz");
+	}
+}
+
+int main()
+{
+	
+	char palabras[10][50];
+	cargar(palabras);
+	imprimir(palabras);
+	consulta(palabras);
+	getch();
+	return 0;
+}
